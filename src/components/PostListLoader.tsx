c507ad7d6264dcfccedb509c51e583406472abcb
kisudeve <kisudeve@gmail.com>
2025-09-25 02:24:07 +0900

    initialized

--- src/components/PostListLoader.tsx
+++ src/components/PostListLoader.tsx
@@ -2,31 +2,39 @@ export default function PostListLoader() {
   return (
     <>
       <section className="grid md:grid-cols-2 gap-[35px] mt-[80px] mb-[152px] animate-pulse">
-        <div>
-          <article>
-            <div className="h-64 bg-gray-300"></div>
-            <div>
-              <div className="w-[73px] h-[26px] bg-gray-200 mt-[21px] mb-[8px]"></div>
-              <div className="h-6 bg-gray-200 mb-[12px]"></div>
-              <div className="h-4 bg-gray-200"></div>
-              <div className="mt-[15px] h-4 bg-gray-200"></div>
-              <div className="mt-4 h-6 bg-gray-200 rounded-full w-6"></div>
-              <div className="h-4 bg-gray-200 w-1/4 mt-1"></div>
-            </div>
-          </article>
+        <div className="bg-white">
+          <a href="#" aria-label="Loading article">
+            <article className="animate-pulse">
+              <div className="bg-gray-200 w-full py-[25%] rounded-lg"></div>
+              <div className="mt-5.25">
+                <div className="bg-gray-300 w-20 h-6.5 rounded-md mb-4"></div>
+                <div className="bg-gray-300 w-3/4 h-6 rounded-md mb-3"></div>
+                <div className="bg-gray-300 w-1/2 h-4 rounded-md mb-2"></div>
+                <div className="bg-gray-300 w-full h-4 rounded-md mb-4"></div>
+                <div className="flex items-center gap-4 mt-4">
+                  <div className="bg-gray-200 w-10 h-10 rounded-full"></div>
+                  <div className="bg-gray-300 w-24 h-4 rounded-md"></div>
+                </div>
+              </div>
+            </article>
+          </a>
         </div>
-        <div>
-          <article>
-            <div className="h-64 bg-gray-300"></div>
-            <div>
-              <div className="w-[73px] h-[26px] bg-gray-200 mt-[21px] mb-[8px]"></div>
-              <div className="h-6 bg-gray-200 mb-[12px]"></div>
-              <div className="h-4 bg-gray-200"></div>
-              <div className="mt-[15px] h-4 bg-gray-200"></div>
-              <div className="mt-4 h-6 bg-gray-200 rounded-full w-6"></div>
-              <div className="h-4 bg-gray-200 w-1/4 mt-1"></div>
-            </div>
-          </article>
+        <div className="bg-white">
+          <a href="#" aria-label="Loading article">
+            <article className="animate-pulse">
+              <div className="bg-gray-200 w-full py-[25%] rounded-lg"></div>
+              <div className="mt-5.25">
+                <div className="bg-gray-300 w-20 h-6.5 rounded-md mb-4"></div>
+                <div className="bg-gray-300 w-3/4 h-6 rounded-md mb-3"></div>
+                <div className="bg-gray-300 w-1/2 h-4 rounded-md mb-2"></div>
+                <div className="bg-gray-300 w-full h-4 rounded-md mb-4"></div>
+                <div className="flex items-center gap-4 mt-4">
+                  <div className="bg-gray-200 w-10 h-10 rounded-full"></div>
+                  <div className="bg-gray-300 w-24 h-4 rounded-md"></div>
+                </div>
+              </div>
+            </article>
+          </a>
         </div>
       </section>
     </>
